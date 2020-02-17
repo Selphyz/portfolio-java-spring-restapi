3efa8beadea7d9af644a2703711dc351bf5e591f
Carlos Dominguez <carlos.dom13@hotmail.com>
2020-02-17 13:30:09 +0100

    17 febrero mediodia

--- src/main/java/com/example/demoapirest/models/dao/IClienteDao.java
+++ src/main/java/com/example/demoapirest/models/dao/IClienteDao.java
@@ -1,7 +1,7 @@
 package com.example.demoapirest.models.dao;
 
 import com.example.demoapirest.models.entity.Cliente;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
 }
--- src/main/java/com/example/demoapirest/models/services/IClienteService.java
+++ src/main/java/com/example/demoapirest/models/services/IClienteService.java
@@ -1,11 +1,14 @@
 package com.example.demoapirest.models.services;
 
 import com.example.demoapirest.models.entity.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IClienteService {
   public List<Cliente> findAll();
+  public Page<Cliente> findAll(Pageable pageable);
   public Cliente findById(Long id);
   public Cliente save(Cliente cliente);
   public void delete(Long id);
